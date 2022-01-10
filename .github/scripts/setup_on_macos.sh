05ac348388f4db66227a92c2624e1f217c48902b
Ben Armstead <contact@benarmstead.co.uk>
2022-01-10 18:52:54 +0000

    Delete .github/scripts directory

--- .github/scripts/setup_on_macos.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -euxo pipefail
-
-# See: https://flutter.dev/desktop#additional-macos-requirements
-
-# See: https://flutter.dev/desktop#set-up
-flutter config --enable-macos-desktop
--- .github/scripts/setup_on_windows.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -euxo pipefail
-
-# See: https://flutter.dev/desktop#additional-windows-requirements
-
-# See: https://flutter.dev/desktop#set-up
-flutter config --enable-windows-desktop
